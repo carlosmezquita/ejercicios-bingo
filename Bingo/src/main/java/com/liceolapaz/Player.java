66655c163ccb2cc7a419ff32c844c7c65e1955b4
Ales <aparff@gmail.com>
2022-11-14 13:26:11 +0100

    Player atribute Bingo added

--- Bingo/src/main/java/com/liceolapaz/Player.java
+++ Bingo/src/main/java/com/liceolapaz/Player.java
@@ -7,11 +7,13 @@ public class Player implements Runnable {
     private ArrayList<Card> card;
     private int credit;
     private String name;
+    private Bingo bingo;
 
-    public Player(ArrayList<Card> card, String name, int credit) {
+    public Player(ArrayList<Card> card, String name, int credit, Bingo bingo) {
         this.card = getCard();
         this.credit = credit;
         this.name = name;
+        this.bingo = bingo;
     }
 
     private ArrayList<Card> getCard() {
