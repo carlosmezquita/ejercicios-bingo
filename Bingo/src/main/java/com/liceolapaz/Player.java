0d92c70e31a6a3e3000de700866558b939fe5727
Alesstintor <aparff@gmail.com>
2022-11-14 21:28:18 +0100

    Player card obtaining

--- Bingo/src/main/java/com/liceolapaz/Player.java
+++ Bingo/src/main/java/com/liceolapaz/Player.java
@@ -4,22 +4,22 @@ import java.util.ArrayList;
 
 public class Player implements Runnable {
 
-    private ArrayList<Card> card;
+    private ArrayList<int[][]> cards;
     private int credit;
     private String name;
     private Bingo bingo;
 
     public Player(ArrayList<Card> card, String name, int credit, Bingo bingo) {
-        this.card = getCard();
+        this.cards = getCard();
         this.credit = credit;
         this.name = name;
         this.bingo = bingo;
     }
 
-    private ArrayList<Card> getCard() {
-        ArrayList<Card> cards = new ArrayList<>();
+    private ArrayList<int[][]> getCard() {
+        ArrayList<int[][]> cards = new ArrayList<>();
         for (int i = 0; i < credit/2; i++) {
-            cards.add(new Card());
+            cards.add(new Card().getCardArray());
         }
         return cards;
     }
