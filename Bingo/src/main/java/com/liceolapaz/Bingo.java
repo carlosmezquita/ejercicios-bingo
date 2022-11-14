46ec7253b6203c85c0a7454d77c0803043a602c6
Ales <aparff@gmail.com>
2022-11-14 13:31:53 +0100

    Bingo generation start

--- Bingo/src/main/java/com/liceolapaz/Bingo.java
+++ Bingo/src/main/java/com/liceolapaz/Bingo.java
@@ -1,12 +1,15 @@
 package com.liceolapaz;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Bingo implements Runnable {
 
     private ArrayList<Integer> totalNumbers;
     private ArrayList<Integer> numbers = new ArrayList<Integer>();
     private final int price = 2;
+    private boolean bingo = false;
+    private boolean line = false;
 
     public Bingo() {
         this.totalNumbers = new ArrayList<>();
@@ -17,6 +20,14 @@ public class Bingo implements Runnable {
 
     @Override
     public void run() {
+        while (!bingo) {
+            int number = totalNumbers.get(genNumber(totalNumbers.size() - 1));
+            numbers.add(number);
+        }
 
+    }
+
+    private int genNumber(int max){
+        return new Random().nextInt(max);
     }
 }
